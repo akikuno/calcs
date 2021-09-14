18e3049edbfe0d0422a82fb6c3b2442464c70b28
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-14 10:55:11 +0900

    Update

--- src/calcs/calcs.py
+++ src/calcs/calcs.py
@@ -1,18 +1,39 @@
-import subprocess
 import pysam
+import argparse
+import sys
 
 
-for x in dir(pysam):
-    print(x, ':', type(eval("pysam."+x)))
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('infile', nargs='?', type=argparse.FileType(),
+                        default=sys.stdin)
+    args = parser.parse_args()
 
-samfile = pysam.AlignmentFile("tests/deletion/test_del.sam", "r")
-samfile
+    file = []
+    with args.infile as f:
+        for s in f:
+            row = s.strip().split("\t")
+            file.append(row)
+        print(file)
 
-pysam.calmd("tests/deletion/test_del.sam", "tests/deletion/test_ref.fa")
 
-pysam.sort("-o", "tmp.bam", "tests/deletion/test_del.sam")
+if __name__ == "__main__":
+    main()
 
-subprocess.run(
-    ["samtools", "calmd", "tests/deletion/test_del.sam", "tests/deletion/test_ref.fa"])
+# parser = argparse.ArgumentParser(
+#     description='このプログラムの説明（なくてもよい）')    # 2. パーサを作る
 
-pysam.calmd
+# for x in dir(pysam):
+#     print(x, ':', type(eval("pysam."+x)))
+
+# samfile = pysam.AlignmentFile("tests/deletion/test_del.sam", "r")
+# samfile
+
+# pysam.calmd("tests/deletion/test_del.sam", "tests/deletion/test_ref.fa")
+
+# pysam.sort("-o", "tmp.bam", "tests/deletion/test_del.sam")
+
+# subprocess.run(
+#     ["samtools", "calmd", "tests/deletion/test_del.sam", "tests/deletion/test_ref.fa"])
+
+# pysam.calmd
