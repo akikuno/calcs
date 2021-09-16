eaad6da93f09b1ed170283682acf732bcb202f2d
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-16 09:09:32 +0900

    update tests

--- tests/deletion/generate_del.sh
+++ tests/deletion/generate_del.sh
@@ -6,5 +6,6 @@ que=tests/deletion/del.fq
 minimap2 --cs "$ref" "$que" >tests/deletion/del_cs.paf
 minimap2 --cs=long "$ref" "$que" >tests/deletion/del_cslong.paf
 
+minimap2 -ax map-ont "$ref" "$que" >tests/deletion/del.sam
 minimap2 -ax map-ont --cs "$ref" "$que" >tests/deletion/del_cs.sam
 minimap2 -ax map-ont --cs=long "$ref" --cs "$que" >tests/deletion/del_cslong.paf
--- tests/substitution/generate_sub.sh
+++ tests/substitution/generate_sub.sh
@@ -6,5 +6,6 @@ que=tests/substitution/sub.fq
 minimap2 --cs "$ref" "$que" >tests/substitution/sub_cs.paf
 minimap2 --cs=long "$ref" "$que" >tests/substitution/sub_cslong.paf
 
+minimap2 -ax map-ont "$ref" "$que" >tests/substitution/sub.sam
 minimap2 -ax map-ont --cs "$ref" "$que" >tests/substitution/sub_cs.sam
 minimap2 -ax map-ont --cs=long "$ref" --cs "$que" >tests/substitution/sub_cslong.paf
