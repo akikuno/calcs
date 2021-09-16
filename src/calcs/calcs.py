51c3d8620b45a9f0a2dcac78b98e2f17feb47831
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-16 09:09:47 +0900

    parse query_sam

--- src/calcs/calcs.py
+++ src/calcs/calcs.py
@@ -1,6 +1,6 @@
 # from parser import parser
 from itertools import compress
-import formatter
+# import formatter
 import argparse
 import sys
 
@@ -18,15 +18,25 @@ parser.add_argument("-@", "--threads", default=1, type=int,
                     help="Number of threads [default: 1]")
 args = parser.parse_args()
 
-
-query = []
-with args.query as f:
+# *TEST========================================================
+# arg.query=open("tests/substitution/sub.sam")
+query_sam = []
+with open("tests/deletion/del.sam") as f:
     for s in f:
         row = s.strip()
-        query.append(row)
+        query_sam.append(row)
 
-header = [s.startswith("@") for s in query]
-print(list(compress(query, header)))
+header = [_.startswith("@") for _ in query_sam]
+body = [not _ for _ in header]
+
+header = list(compress(query_sam, header))
+body = list(compress(query_sam, body))
+
+cigar = [s.split("\t")[5] for s in body]
+query = [s.split("\t")[9] for s in body]
+
+
+# *TEST========================================================
 
 # print(query)
 
