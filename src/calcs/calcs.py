75371c51f1ccaa064bf766f4b98a99868cf151dc
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-16 16:31:08 +0900

    read reference fasta

--- src/calcs/calcs.py
+++ src/calcs/calcs.py
@@ -4,37 +4,47 @@ from itertools import compress
 import argparse
 import sys
 
-parser = argparse.ArgumentParser()
-parser.add_argument('query', nargs="?", type=argparse.FileType("r"),
-                    default=sys.stdin,
-                    help="Give the full path to a SAM file")
-parser.add_argument("-r", "--reference", required=True,
-                    help="Give the full path to a reference FASTA file")
-parser.add_argument("-l", "--long", action="store_true",
-                    help="Output the cs tag in the long form")
-parser.add_argument("-p", "--paf", action="store_true",
-                    help="Output PAF")
-parser.add_argument("-@", "--threads", default=1, type=int,
-                    help="Number of threads [default: 1]")
-args = parser.parse_args()
+# parser = argparse.ArgumentParser()
+# parser.add_argument('query', nargs="?", type=argparse.FileType("r"),
+#                     default=sys.stdin,
+#                     help="Give the full path to a SAM file")
+# parser.add_argument("-r", "--reference", required=True,
+#                     help="Give the full path to a reference FASTA file")
+# parser.add_argument("-l", "--long", action="store_true",
+#                     help="Output the cs tag in the long form")
+# parser.add_argument("-p", "--paf", action="store_true",
+#                     help="Output PAF")
+# parser.add_argument("-@", "--threads", default=1, type=int,
+#                     help="Number of threads [default: 1]")
+# args = parser.parse_args()
 
 # *TEST========================================================
-# arg.query=open("tests/substitution/sub.sam")
-query_sam = []
+# arg.query=open("tests/deletion/del.sam")
+# arg.reference="tests/random_100bp.fa"
+
+que_sam = []
 with open("tests/deletion/del.sam") as f:
     for s in f:
         row = s.strip()
-        query_sam.append(row)
+        que_sam.append(row)
 
-header = [_.startswith("@") for _ in query_sam]
-body = [not _ for _ in header]
+header_idx = [_.startswith("@") for _ in que_sam]
+body_idx = [not _ for _ in header_idx]
 
-header = list(compress(query_sam, header))
-body = list(compress(query_sam, body))
+header = list(compress(que_sam, header_idx))
+body = list(compress(que_sam, body_idx))
 
 cigar = [s.split("\t")[5] for s in body]
-query = [s.split("\t")[9] for s in body]
+que_seq = [s.split("\t")[9] for s in body]
 
+ref_fasta = []
+with open("tests/random_100bp.fa") as f:
+    for s in f:
+        row = s.strip()
+        ref_fasta.append(row)
+
+ref_seq_idx = [not _.startswith(">") for _ in ref_fasta]
+ref_seq = list(compress(ref_fasta, ref_seq_idx))
 
 # *TEST========================================================
 
