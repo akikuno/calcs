d1f1b2447029b7b496e9628405296efb9bde8ff7
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-17 21:08:26 +0900

    WIP: Annotate Insertion in reference

--- src/calcs/calcs.py
+++ src/calcs/calcs.py
@@ -99,13 +99,13 @@ que_seq_clipped
 tmp_ref_seq: str = "AAATTT"
 cigar_str: str = "3M2I2D3M"
 expected = "AAAIITTT"
-
 tmp_ref_seq[:3] + int(2) * "I" + tmp_ref_seq[3:6]
 
+# cigar_split = re.split("([0-9]+I)", cigar_str)
 
-cigar_split = re.split("([0-9]+I)", cigar_str)
-cigar_expand = []
-append = cigar_expand.append
+ref_seq_ins_annotated = []
+append = ref_seq_ins_annotated.append
+start_idx: int = 0
 for _cigar in cigar_split:
     if "S" in _cigar or "H" in _cigar:
         pass
@@ -113,36 +113,13 @@ for _cigar in cigar_split:
         ins_num = int(_cigar.replace("I", ""))
         append("I" * ins_num)
     else:
+        print(start_idx)
         cigar_num = int(re.sub("[MDNSHPX=]", "", _cigar))
-        append("=" * cigar_num)
+        end_idx = start_idx + cigar_num
+        append(tmp_ref_seq[start_idx or None:end_idx or None])
+        start_idx += cigar_num
 
-cigar_expand = ''.join(cigar_expand)
-
-ref_seq_ins = []
-append = ref_seq_ins.append
-_ref_retain: str = ""
-for _ref, _cigar in zip_longest(list(tmp_ref_seq), list(cigar_expand)):
-    if _ref_retain and _ref_retain == "=":
-        append(_ref)
-        _ref_retain = ""
-    elif _cigar == "=":
-        append(_ref)
-    elif _cigar == "I":
-        append("I")
-        _ref_retain += _ref
-
-hoge = "jpog"
-if not hoge:
-    print("hoge")
-
-tmp_ref_seq
-cigar_expand
-
-cigar_split = re.split("[MDNSHPX=]", cigar_str)
-cigar_split = re.split("[MDNSHPX=]", cigar_str)
-cigar_split = re.split(r"([0-9]+[A-Z])", cigar_str)
-
-cigar_split[3]
+ref_seq_ins_annotated = ''.join(ref_seq_ins_annotated)
 
 ###############################################################################
 # Annotate Deletion in query
