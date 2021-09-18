920043b455bee7ead8db998ad3f3e5eeab75f202
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-18 14:49:25 +0900

    move code to if __name == __main__

--- src/calcs/calcs.py
+++ src/calcs/calcs.py
@@ -1,3 +1,7 @@
+###############################################################################
+# Import modules
+###############################################################################
+
 import argparse
 import os
 import re
@@ -46,19 +50,6 @@ def read_query_sam(args_query):
             append(row)
     return que_sam
 
-
-QUESAM = tuple(read_query_sam(ARGS_QUERY))
-
-is_header = [_.startswith("@") for _ in QUESAM]
-is_alignment = [not _ for _ in is_header]
-
-HEADER = tuple(list(compress(QUESAM, is_header)))
-ALIGNMENTS = tuple(list(compress(QUESAM, is_alignment)))
-
-STARTS = tuple([int(s.split("\t")[3]) - 1 for s in ALIGNMENTS])
-CIGARS = tuple([s.split("\t")[5] for s in ALIGNMENTS])
-QUESEQS = tuple([s.split("\t")[9] for s in ALIGNMENTS])
-
 ###############################################################################
 # Parse reference FASTA
 ###############################################################################
@@ -74,11 +65,6 @@ def read_reference_fasta(args_reference):
     return ref_fasta
 
 
-REFFASTA = tuple(read_reference_fasta(ARGS_REFERENCE))
-is_alignment = [not _.startswith(">") for _ in REFFASTA]
-
-REFSEQS = tuple(list(compress(REFFASTA, is_alignment)) * len(QUESEQS))
-
 ###############################################################################
 # Trim start sites in reference
 ###############################################################################
@@ -86,11 +72,6 @@ REFSEQS = tuple(list(compress(REFFASTA, is_alignment)) * len(QUESEQS))
 
 def trim_starts(ref_seq: str, start: int) -> str:
     return ref_seq[start or None:]
-
-
-with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-    _ = list(executor.map(trim_starts, REFSEQS, STARTS))
-    REFSEQS_TRIMMED = tuple(_)
 
 ###############################################################################
 # Trim soft/hard clip in query
@@ -112,17 +93,9 @@ def len_clips(cigar):
     return left_length, right_length
 
 
-LEN_CLIPS = list(map(len_clips, CIGARS))
-
-
 def trim_queseqs(queseq, len_clip):
     left, right = len_clip
     return queseq[left or None: -right or None]
-
-
-with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-    _ = list(executor.map(trim_queseqs, QUESEQS, LEN_CLIPS))
-    QUESEQS_TRIMMED = tuple(_)
 
 ###############################################################################
 # Annotate Insertion in reference
@@ -148,10 +121,6 @@ def annotate_insertion(ref, cigar):
     return ''.join(annotates)
 
 
-with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-    _ = list(executor.map(annotate_insertion, REFSEQS_TRIMMED, CIGARS))
-    REFSEQS_ANNO = tuple(_)
-
 ###############################################################################
 # Annotate Deletion in query
 ###############################################################################
@@ -174,11 +143,6 @@ def annotate_deletion(que, cigar):
             append(que[start_idx or None:end_idx or None])
             start_idx += cigar_num
     return ''.join(annotates)
-
-
-with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-    _ = list(executor.map(annotate_deletion, QUESEQS_TRIMMED, CIGARS))
-    QUESEQS_ANNO = tuple(_)
 
 
 ###############################################################################
@@ -228,83 +192,127 @@ def call_cs_short(cslong):
         append(_cs)
     return ''.join(cs)
 
-
-with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-    cstags = list(executor.map(call_cs_long, REFSEQS_ANNO, QUESEQS_ANNO))
-
-
-if ARGS_LONG:
-    CSTAGS = tuple(cstags)
-else:
-    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-        _ = list(executor.map(call_cs_short, cstags))
-        CSTAGS = tuple(_)
-
-
 ###############################################################################
 # Output SAM or PAF
 ###############################################################################
-
 # PAF format:
 # https://github.com/lh3/miniasm/blob/master/PAF.md
 
-if ARGS_PAF:
-    def determine_strand(flag: int) -> str:
-        _cache = flag
-        _strand = "+"
-        while _cache > 0:
-            _power = int(log2(_cache))
-            _cache = _cache - 2**_power
-            if _power == 4:
-                _strand = "-"
-                break
-        return _strand
 
-    def convert_to_paf(alignment, cstag, len_clip, start, refseq, refseq_anno) -> str:
-        alignment_fields = alignment.split("\t")
-        start_clips, end_clips = len_clip
-        _quename = alignment_fields[0]
-        _quelen = str(len(alignment_fields[9]))
-        _questart = str(start_clips)
-        _queend = str(len(alignment_fields[9]) - end_clips)
-        _strand = determine_strand(int(alignment_fields[1]))
-        _refname = alignment_fields[2]
-        _reflen = str(len(refseq))
-        _refstart = str(start)
-        _refend = str(len(refseq_anno.replace("I", "")) + start)
-        if "cs:Z:=" in cstag:
-            _matches = len(re.findall('[A-Z]', cstag)) - 1
-        else:
-            cs_split = re.split(r"[a-zA-Z:\-+*]", cstag)
-            _matches = sum([int(s or 0) for s in cs_split])
-        _matches = str(_matches)
-        _blocklen = str(len(refseq_anno))
-        _quality = alignment_fields[4]
-        _others = alignment_fields[11:]
-        _others.append(cstag)
-        paf = [_quename, _quelen, _questart, _queend, _strand, _refname,
-               _reflen, _refstart, _refend, _matches, _blocklen, _quality]
-        return '\t'.join(paf + _others)
+def determine_strand(flag: int) -> str:
+    _cache = flag
+    _strand = "+"
+    while _cache > 0:
+        _power = int(log2(_cache))
+        _cache = _cache - 2**_power
+        if _power == 4:
+            _strand = "-"
+            break
+    return _strand
 
-    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-        paf_cstags = list(executor.map(convert_to_paf,
-                                       ALIGNMENTS, CSTAGS, LEN_CLIPS,
-                                       STARTS, REFSEQS, REFSEQS_ANNO))
-        sys.stdout.write('\n'.join(paf_cstags + [""]))
-else:
-    def insert_cstag(alignment: str, cstag: str) -> str:
-        return re.sub("(\trl:i:0)", "\t" + cstag + r"\1", alignment)
 
-    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
-        alignment_cstags = list(executor.map(insert_cstag, ALIGNMENTS, CSTAGS))
-    SAM_CSTAGS = HEADER + tuple(alignment_cstags)
-    sys.stdout.write('\n'.join(SAM_CSTAGS + ("",)))
+def convert_to_paf(alignment, cstag, len_clip, start, refseq, refseq_anno) -> str:
+    alignment_fields = alignment.split("\t")
+    start_clips, end_clips = len_clip
+    _quename = alignment_fields[0]
+    _quelen = str(len(alignment_fields[9]))
+    _questart = str(start_clips)
+    _queend = str(len(alignment_fields[9]) - end_clips)
+    _strand = determine_strand(int(alignment_fields[1]))
+    _refname = alignment_fields[2]
+    _reflen = str(len(refseq))
+    _refstart = str(start)
+    _refend = str(len(refseq_anno.replace("I", "")) + start)
+    if "cs:Z:=" in cstag:
+        _matches = len(re.findall('[A-Z]', cstag)) - 1
+    else:
+        cs_split = re.split(r"[a-zA-Z:\-+*]", cstag)
+        _matches = sum([int(s or 0) for s in cs_split])
+    _matches = str(_matches)
+    _blocklen = str(len(refseq_anno))
+    _quality = alignment_fields[4]
+    _others = alignment_fields[11:]
+    _others.append(cstag)
+    paf = [_quename, _quelen, _questart, _queend, _strand, _refname,
+           _reflen, _refstart, _refend, _matches, _blocklen, _quality]
+    return '\t'.join(paf + _others)
+
+
+def insert_cstag(alignment: str, cstag: str) -> str:
+    return re.sub("(\trl:i:0)", "\t" + cstag + r"\1", alignment)
+
+
+###############################################################################
+# MAIN
+###############################################################################
 
 
 if __name__ == "__main__":
-    # query, reference, long, paf, threads = parser()
-    print(args.query)
-    print(args.reference)
-    print(args.long)
-    print(args.paf)
-    print(args.threads)
+    # Argument parse
+    ARGS_QUERY, ARGS_REFERENCE, ARGS_LONG, ARGS_PAF, ARGS_THREADS = parse_args()
+
+    # Parse Query SAM file
+    QUESAM = tuple(read_query_sam(ARGS_QUERY))
+
+    is_header = [_.startswith("@") for _ in QUESAM]
+    is_alignment = [not _ for _ in is_header]
+
+    HEADER = tuple(list(compress(QUESAM, is_header)))
+    ALIGNMENTS = tuple(list(compress(QUESAM, is_alignment)))
+
+    STARTS = tuple([int(s.split("\t")[3]) - 1 for s in ALIGNMENTS])
+    CIGARS = tuple([s.split("\t")[5] for s in ALIGNMENTS])
+    QUESEQS = tuple([s.split("\t")[9] for s in ALIGNMENTS])
+
+    # Parse Reference FASTA file
+    REFFASTA = tuple(read_reference_fasta(ARGS_REFERENCE))
+    is_alignment = [not _.startswith(">") for _ in REFFASTA]
+
+    REFSEQS = tuple(list(compress(REFFASTA, is_alignment)) * len(QUESEQS))
+
+    # Trim start sites in reference sequence
+    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+        _ = list(executor.map(trim_starts, REFSEQS, STARTS))
+        REFSEQS_TRIMMED = tuple(_)
+
+    # Trim soft/hard clip into query sequence
+    LEN_CLIPS = list(map(len_clips, CIGARS))
+
+    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+        _ = list(executor.map(trim_queseqs, QUESEQS, LEN_CLIPS))
+        QUESEQS_TRIMMED = tuple(_)
+
+    # Annotate Insertion in reference sequence
+    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+        _ = list(executor.map(annotate_insertion, REFSEQS_TRIMMED, CIGARS))
+        REFSEQS_ANNO = tuple(_)
+
+    # Annotate Deletion into query seuqence
+    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+        _ = list(executor.map(annotate_deletion, QUESEQS_TRIMMED, CIGARS))
+        QUESEQS_ANNO = tuple(_)
+
+    # Calculate CS tags
+    with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+        cstags = list(executor.map(call_cs_long, REFSEQS_ANNO, QUESEQS_ANNO))
+
+    if ARGS_LONG:
+        CSTAGS = tuple(cstags)
+    else:
+        with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+            _ = list(executor.map(call_cs_short, cstags))
+            CSTAGS = tuple(_)
+
+    # Output PAF or SAM
+    if ARGS_PAF:
+        with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+            paf_cstags = list(executor.map(convert_to_paf,
+                                           ALIGNMENTS, CSTAGS, LEN_CLIPS,
+                                           STARTS, REFSEQS, REFSEQS_ANNO))
+            sys.stdout.write('\n'.join(paf_cstags + [""]))
+    else:
+        with ProcessPoolExecutor(max_workers=ARGS_THREADS) as executor:
+            alignment_cstags = list(executor.map(
+                insert_cstag, ALIGNMENTS, CSTAGS))
+        SAM_CSTAGS = HEADER + tuple(alignment_cstags)
+        sys.stdout.write('\n'.join(SAM_CSTAGS + ("",)))
