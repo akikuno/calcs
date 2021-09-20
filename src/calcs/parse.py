f05b8a02ddbf151f8482d00bc4ea221f6a7a8d5c
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-09-21 08:07:39 +0900

    add metavar

--- src/calcs/parse.py
+++ src/calcs/parse.py
@@ -5,17 +5,25 @@ import sys
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('query', nargs="?", type=argparse.FileType("r"),
+    parser.add_argument('query', nargs="?",
+                        type=argparse.FileType("r"),
                         default=sys.stdin,
+                        metavar="<in.sam>",
                         help="Give the full path to a SAM file")
     parser.add_argument("-r", "--reference", required=True,
+                        metavar="<in.fasta>",
                         help="Give the full path to a reference FASTA file")
-    parser.add_argument("-l", "--long", action="store_true",
+    parser.add_argument("-l", "--long",
+                        action="store_true",
                         help="Output the cs tag in the long form")
-    parser.add_argument("-p", "--paf", action="store_true",
+    parser.add_argument("-p", "--paf",
+                        action="store_true",
                         help="Output PAF")
-    parser.add_argument("-t", "--threads", default=1, type=int,
-                        help="Number of threads [default: 1]")
+    parser.add_argument("-t", "--threads",
+                        default=1,
+                        type=int,
+                        metavar="threads",
+                        help="Number of threads [default: 1]",)
     args = parser.parse_args()
     os_cpus = int(os.cpu_count())  # len(os.sched_getaffinity(0))
     if args.threads > os_cpus:
